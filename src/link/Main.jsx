c146bfa57c1a995092b828a2f19f907170cae85c
Гиматдинов Равиль Рустемович <241905@mck-ktits.ru>
2023-01-27 13:54:51 +0300

    Первая инициализация проекта

---
+++ src/link/Main.jsx
@@ -0,0 +1,251 @@
+function Main() {
+  
+    return (
+
+        <div className="main">
+            <div className="header_main"> 
+                <div className="logo">
+                    <h2> Food<span>House </span> </h2>
+                </div>
+                <div className="nav_menu">
+                    <div className="sector">
+                        <a href="#"> Главная </a>
+                    </div>
+                    <div className="sector">
+                        <a href="#"> Меню </a>
+                    </div>
+                    <div className="sector">
+                        <a href="#"> Сервис </a>
+                    </div>
+                    <div className="sector">
+                        <a href="#"> О нас </a>
+                    </div>
+                </div>
+                <div className="icons">
+                    <img src="img/cart-icon.svg" />
+                </div>
+            </div>
+
+            <div className="banner">
+                <div className="left_banner">
+                    <p> Экспресс-доставка от Яндекс.Еды </p>
+                        <h2> Быстро <br /> и вкусно<span1>.</span1> </h2>
+                            <span> Сеть быстрого питания #1 в Казани </span>
+                    <div className="buts_banner">
+                        <a href="#"><div className="but">
+                            <p> Сделать заказ </p>
+                        </div></a>
+
+                        <div className="text_deliver">
+                            <p> бесплатная доставка <br /> от 500 рублей </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="right_banner">
+                    <img src="img/banner.png"/>
+                </div>
+            </div>
+            <div className="menu"> 
+
+                <div className="header_menu">
+                    <p> Наше Меню </p>
+                    <div className="polosa"></div>
+                </div>
+                <div className="menu_section">
+
+                    <div className="section">
+                        <img src="img/cup.svg" />
+                        <p> Салаты</p>
+                    </div>
+                    
+                    <div className="section">
+                        <img src="img/cup.svg" />
+                        <p> Напитки </p>
+                    </div>
+
+                    <div className="section">
+                        <img src="img/cup.svg" />
+                        <p> Бургеры </p>
+                    </div>
+
+                    <div className="section">
+                        <img src="img/cup.svg" />
+                        <p> Закуски </p>
+                    </div>
+
+                    <div className="section">
+                        <img src="img/cup.svg" />
+                        <p> Пицца </p>
+                    </div>
+
+                    <div className="section">
+                        <img src="img/cup.svg" />
+                        <p> Акции </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="products_con">
+                <div className="products">
+
+                    <div className="product">
+                        <div className="img_product">
+                            <img src="img/product2.png"/>
+                        </div>
+                        <div className="product_description">
+                            <div className="name_product">
+                                <p> Салат "Греческий" </p>
+                            </div>
+                            <div className="portion">
+                                <p> 300 грамм - 1 порция </p>
+                            </div>
+                            <div className="composition">
+                                <p> 36 - белков, 11 жиров, 55 - углеводов </p>
+                            </div>
+
+                            <div className="but_product">
+                                <p> Добавить в корзину </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="product">
+                        <div className="img_product">
+                            <img src="img/product2.png"/>
+                        </div>
+                        <div className="product_description">
+                            <div className="name_product">
+                                <p> Салат "Греческий" </p>
+                            </div>
+                            <div className="portion">
+                                <p> 300 грамм - 1 порция </p>
+                            </div>
+                            <div className="composition">
+                                <p> 36 - белков, 11 жиров, 55 - углеводов </p>
+                            </div>
+
+                            <div className="but_product">
+                                <p> Добавить в корзину </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="product">
+                        <div className="img_product">
+                            <img src="img/product2.png"/>
+                        </div>
+                        <div className="product_description">
+                            <div className="name_product">
+                                <p> Салат "Греческий" </p>
+                            </div>
+                            <div className="portion">
+                                <p> 300 грамм - 1 порция </p>
+                            </div>
+                            <div className="composition">
+                                <p> 36 - белков, 11 жиров, 55 - углеводов </p>
+                            </div>
+
+                            <div className="but_product">
+                                <p> Добавить в корзину </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="product">
+                        <div className="img_product">
+                            <img src="img/product2.png"/>
+                        </div>
+                        <div className="product_description">
+                            <div className="name_product">
+                                <p> Салат "Греческий" </p>
+                            </div>
+                            <div className="portion">
+                                <p> 300 грамм - 1 порция </p>
+                            </div>
+                            <div className="composition">
+                                <p> 36 - белков, 11 жиров, 55 - углеводов </p>
+                            </div>
+
+                            <div className="but_product">
+                                <p> Добавить в корзину </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="product">
+                        <div className="img_product">
+                            <img src="img/product2.png"/>
+                        </div>
+                        <div className="product_description">
+                            <div className="name_product">
+                                <p> Салат "Греческий" </p>
+                            </div>
+                            <div className="portion">
+                                <p> 300 грамм - 1 порция </p>
+                            </div>
+                            <div className="composition">
+                                <p> 36 - белков, 11 жиров, 55 - углеводов </p>
+                            </div>
+
+                            <div className="but_product">
+                                <p> Добавить в корзину </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="product">
+                        <div className="img_product">
+                            <img src="img/product2.png"/>
+                        </div>
+                        <div className="product_description">
+                            <div className="name_product">
+                                <p> Салат "Греческий" </p>
+                            </div>
+                            <div className="portion">
+                                <p> 300 грамм - 1 порция </p>
+                            </div>
+                            <div className="composition">
+                                <p> 36 - белков, 11 жиров, 55 - углеводов </p>
+                            </div>
+
+                            <div className="but_product">
+                                <p> Добавить в корзину </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="product">
+                        <div className="img_product">
+                            <img src="img/product2.png"/>
+                        </div>
+                        <div className="product_description">
+                            <div className="name_product">
+                                <p> Салат "Греческий" </p>
+                            </div>
+                            <div className="portion">
+                                <p> 300 грамм - 1 порция </p>
+                            </div>
+                            <div className="composition">
+                                <p> 36 - белков, 11 жиров, 55 - углеводов </p>
+                            </div>
+
+                            <div className="but_product">
+                                <p> Добавить в корзину </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <a href="#"><div className="but_more">
+                    Показать ещё
+                </div></a>
+
+            </div>
+        </div>
+
+    )
+  }
+  
+  export default Main
